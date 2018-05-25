b40c5ed9072c5c6820d5c425ec3e76425593e48f
mark <murimimg180@gmail.com>
2018-05-25 18:30:43 +0300

    made changes

--- neighbour/hood/views.py
+++ neighbour/hood/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    return render(request, 'home.html')
--- neighbour/neighbour/settings.py
+++ neighbour/neighbour/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hood.apps.HoodConfig',
+    'hood',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
